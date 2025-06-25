4c1a9b611ac9a152abbedbaac1aea6a5bce58807
Kamel Al Aridi <kamelalaridi@gmail.com>
2025-06-25 11:56:48 +0300

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Playlist from "./pages/Playlist";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/playlists/:id" element={<Playlist />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
